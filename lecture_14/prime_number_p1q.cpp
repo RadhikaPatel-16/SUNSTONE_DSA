aa947daa9a34b707a0e8aba6a7a39d38fd70919a
RadhikaPatel-16 <radhiksinghpatel16@gmail.com>
2026-02-04 17:24:31 +0530

    notes

--- lecture_14/prime_number_p1q.cpp
+++ lecture_14/prime_number_p1q.cpp
@@ -1,22 +1,23 @@
 #include<iostream>
-#include<math.h>
+#include<cmath>
 using namespace std;
 int main(){
     int n;
     cin>>n;
     //flag or monitor
     bool ans=true;
-    for(int i=2;i<=sqrt(n);i++)
+    for(int i=2;i<=sqrt(n);i++){
     if (n%i==0){
         ans=false;
         break;
     }
 
 }
-if(!ans){
+if(ans){
     cout<<"prime nahi hai";
 }
 else {
     cout <<"prime hai";
 }
 return 0;
+}
